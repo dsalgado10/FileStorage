60d79ccbf515288309b130d101ed5567038af30a
dsalgado10 <daniel.salgado@devsu.com>
2022-08-25 15:49:40 -0600

    Agregar error handling

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,10 +1,23 @@
-const registerUser = (req, res) => {
-    res.send('Ruta Registro');
-}
+const asyncHandler = require('express-async-handler');
 
-const loginUser = (req, res) => {
+// Registrar un nuevo usuario
+// Ruta: /api/users
+// Permiso: Publico
+const registerUser = asyncHandler (async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Incluya todos los campos')
+    }
+    res.send('Ruta Registro');
+})
+
+// Login de usuario
+// Ruta: /api/login
+// Permiso: Publico
+const loginUser = asyncHandler (async (req, res) => {
     res.send('Ruta Login');
-}
+})
 
 module.exports = {
     registerUser,
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.get('/api/users', (req,res) => {
-    res.status(200).json({message: 'Bienvenido al FileStorage'});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Bienvenido al FileStorage' });
 });
 
 //Rutas usuario
 app.use('/api/users', require('./routes/userRoutes'));
 
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
